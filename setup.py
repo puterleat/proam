3754c57c40475c3b76e410395819e6d19aa3efba
Ben Whalley <benwhalley@gmail.com>
2016-01-16 23:16:25 +0000

    initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+from distutils.core import setup
+
+setup(
+    name='proam',
+    version='0.0.1',
+    author='Ben Whalley',
+    author_email='benwhalley@gmail.com',
+    packages=[],
+    scripts=[
+        'bin/proam',
+    ],
+    url='http://pypi.python.org/pypi/proam/',
+    license='LICENSE.txt',
+    description='Process photos to send to ProAm.',
+    install_requires=[
+        "Pillow>=2.6.1",
+        "click>=6.2",
+    ],
+
+)
