81baf73523f30a7f661f801a2310b5d62647755b
Ben Whalley <benwhalley@gmail.com>
2016-01-18 12:23:23 +0000

    license and readme

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ setup(
     version='0.0.1',
     author='Ben Whalley',
     author_email='benwhalley@gmail.com',
-    packages=[],
+    package_data={'':['*']},
+
     scripts=[
         'bin/proam',
     ],
     url='http://pypi.python.org/pypi/proam/',
     license='LICENSE.txt',
-    description='Process photos to send to ProAm.',
+    description='Process photos to send to ProAm (http://proamimaging.com).',
     install_requires=[
         "Pillow>=2.6.1",
         "click>=6.2",
